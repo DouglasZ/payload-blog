59736b30ee6e25642186369aa9a880a4d1eb262c
Douglas Zilli <douglas.zilli@tglab.com>
2026-02-16 21:55:31 -0300

    feat(seeders): articles

--- src/scripts/seed/script.ts
+++ src/scripts/seed/script.ts
@@ -1,12 +1,14 @@
 import { seedAdmin } from '@/scripts/seed/seeders/admin.seeder'
 import { getPayloadClient } from '@/lib/payload/client'
 import { seedArticleAuthor } from '@/scripts/seed/seeders/article-author.seeder'
+import { seedArticles } from '@/scripts/seed/seeders/articles.seeder'
 
 async function main() {
   const payload = await getPayloadClient()
   try {
     await seedAdmin(payload)
     await seedArticleAuthor(payload)
+    await seedArticles(payload)
     process.exit(0)
   } catch (error) {
     console.error(error)
